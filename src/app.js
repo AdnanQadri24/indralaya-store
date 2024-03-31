4d3afc1828ada8f8237899515e71c497a3a619e2
Denun <adnanalqadri2004@gmail.com>
2024-03-31 12:38:31 +0700

    menghubungkan data barang ke fitur checkout

--- src/app.js
+++ src/app.js
@@ -5,5 +5,28 @@ document.addEventListener('alpine:init', () => {
                 { id : 2, name: 'Samsung HD TV 32" UA32T4001 (2020)', img: 'tv_2.png', price: 2669000},
                 { id : 3, name: 'AC Sharp 1/2 PK', img: 'ac_1.png', price: 3100000},
         ]
-    }))
-})
+    }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem){
+            this.items.push(newItem);
+            this.quantity ++;
+            this.total += newItem.price;
+            console.log(this.total);
+        }
+    });
+
+});
+
+
+// konversi ke Rupiah
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0
+    }).format(number);
+};
