6dff3df954289d8bb3df80ddf8cb5f49220aa116
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-04-01 21:04:17 +0700

    feat(app): add checkout button and midtrans logic
    1. add validation to checkout button
    2. add ajax for fetching the data for midtrans

--- src/app.js
+++ src/app.js
@@ -68,6 +68,47 @@ document.addEventListener('alpine:init', () => {
 
 });
 
+// form validation
+const checkoutButton = document.querySelector('.checkout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function() {
+    for(let i = 0; i < form.elements.length; i++) {
+        if(form.elements[i].value.length !== 0) {
+            checkoutButton.classList.remove('disabled');
+            checkoutButton.classList.add('disabled');
+        } else {
+            return false
+        }
+    }
+
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove('disabled');
+});
+
+// kirim data ketika tombol checkout diklik 
+checkoutButton.addEventListener('click', async function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objData = Object.fromEntries(data);
+    
+    // minta transaction token menggunakan ajax
+    try {
+        const response = await fetch('php/placeOrder.php', {
+            method: 'POST',
+            body: data
+        });
+        const token = await response.text();
+        window.snap.pay(token);
+       //  console.log(token)
+    } catch(err) {
+        console.log(err.message);
+    }
+
+})
 
 // konversi ke Rupiah
 const rupiah = (number) => {
